f919dc2c99691f84b772a5a1187d81ab7ce709b8
Moivvas <moivvas@gmail.com>
2023-08-26 15:10:19 +0300

    Created setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Personal assistant project',
+    version='0.1.0',
+    description='GoIT group project as personal assistant',
+    Team_name='Next frontier',
+    author_email='moivvas@gmail.com',
+    packages=['pers_assist'],
+)
